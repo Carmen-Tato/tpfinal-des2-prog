d402779e172203f60cf198ae52edb80cfdfb0ded
Carmen-Tato <carmentato2.12@gmail.com>
2024-11-13 23:07:10 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,56 @@
+const productos = [
+    {
+        nombre: 'Café variedad Arábica',
+        precio: 15000,
+        img: 'variedad-arabica',
+        descripcion: 'Café tostado en grano o molido x250g',
+    },
+
+    {
+        nombre: 'Café variedad Colombia',
+        precio: 17450,
+        img: 'variedad-colombia',
+        descripcion: 'Café tostado en grano o molido x250g',
+    },
+
+    {
+        nombre: 'Café variedad Brasil',
+        precio: 18000,
+        img: 'variedad-brasil',
+        descripcion: 'Café tostado en grano o molido x250g',
+    },
+    {
+        nombre: 'Blend de especialidad',
+        precio: 23199,
+        img: 'blend',
+        descripcion: 'Café tostado en grano x250g',
+    },
+    {
+        nombre: 'Café sin cafeina',
+        precio: 18000,
+        img: 'sin-cafeina',
+        descripcion: 'Torrado x250g',
+    }, {
+        nombre: 'Café Turco',
+        precio: 24000,
+        img: 'cafe-turco',
+        descripcion: 'Café molido extra fino x250g',
+    }, {
+        nombre: 'Filtro para cafe',
+        precio: 60000,
+        img: 'cafe-de-filtro',
+        descripcion: 'Cafetera de vidrio con filtro',
+    },
+    {
+        nombre: 'Molinillo de cafe',
+        precio: 45000,
+        img: 'molinillo-de-cafe',
+        descripcion: 'Molinillo de café manual',
+    },
+    {
+        nombre: 'Cafetera Italiana',
+        precio: 100000,
+        img: 'Cafetera-italiana',
+        descripcion: 'Cafetera Italiana apta para fuego directo',
+    },
+]
